7c4a6927ac84f9f3b3f8f0ecb387cc2114c15a72
unnati249 <47725121+unnati249@users.noreply.github.com>
2021-06-16 19:37:22 +0530

    Update subscriber.js

--- subscriber.js
+++ subscriber.js
@@ -4,7 +4,7 @@ const mqtt = require('mqtt');
 var client = mqtt.connect("mqtt://broker.hivemq.com");
 
 //local MQTT broker
-var client = mqtt.connect("mqtt://127.0.0.1:1883");
+//var client = mqtt.connect("mqtt://127.0.0.1:1883");
 
 
 client.on('connect',function(){
